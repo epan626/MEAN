a96d3ecf8e913da2870cad13935a5d1e0e5bec24
Eric <epan626@gmail.com>
2017-01-26 08:39:33 -0800

    MEAN stack first comment

--- MEAN/Week_3/Eric_Pan_Archive (1)/client/static/js/controllers/adminControllerC.js
+++
@@ -1,4 +0,0 @@
-app.controller('adminController', ['productFactory', '$scope', '$location', '$cookies', function(productFactory, $scope, $location, $cookies){
-    var cookie = $cookies.get('cookieloggeduser')
-    console.log(cookie)
-}])
